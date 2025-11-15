0cb99b79006935135319125b95080b86808e6bca
szabdam <99809285+szabdam@users.noreply.github.com>
2025-11-15 14:17:01 +0100

    CsomagFeladas.fxml created, fontend package deleted

---
+++ src/main/java/org/example/MainController.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainController {
+    @FXML
+    private void initialize() {
+
+    }
+
+    public void handleFeladasButton(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("CsomagFeladas.fxml"));
+        Scene scene = new Scene(root);
+
+        // Stage lekérése a gomb eseményéből
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void handleAtvetelButton(ActionEvent actionEvent) {
+        System.out.println("Atvetel button clicked");
+    }
+}
+
